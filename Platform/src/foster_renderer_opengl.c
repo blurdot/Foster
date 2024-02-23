25b6212608def04e1d85928eb5cd18c0c07739ec
Nick Schulz <0snick0@gmail.com>
2024-02-23 16:54:34 -0700

    Many multisamples AA

--- Platform/src/foster_renderer_opengl.c
+++ Platform/src/foster_renderer_opengl.c
@@ -972,7 +972,7 @@ void FosterPrepare_OpenGL()
 		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 		SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 		SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
-		SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
+		SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 16);
 	#endif
 }
 
